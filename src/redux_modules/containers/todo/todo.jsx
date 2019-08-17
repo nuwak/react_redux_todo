33857d2fcdf8b6b028ce7338937937729fbf8703
Igor Bozhennikov <mr.igorr@gmail.com>
2019-08-17 20:04:15 +0300

    filters and localstorage

--- src/redux_modules/containers/todo/todo.jsx
+++ src/redux_modules/containers/todo/todo.jsx
@@ -5,16 +5,14 @@ import ToDoList from '../../components/todo-list/todo-list';
 import Footer from '../../components/footer/footer';
 import {connect} from 'react-redux'
 
-import { addTask, removeTask, completeTask} from "../../actions/actionCreator";
+import {addTask, removeTask, completeTask, changeFilter} from "../../actions/actionCreator";
 
 import './todo.css';
-
 
 
 class ToDo extends Component {
 
   state = {
-    activeFilter: 'all',
     'taskText': ''
   }
 
@@ -25,11 +23,11 @@ class ToDo extends Component {
 
   }
 
-  addTask = ({ key }) => {
-    const { taskText } = this.state
+  addTask = ({key}) => {
+    const {taskText} = this.state
 
-    if (taskText.length > 3  && key === 'Enter') {
-      const { addTask } = this.props
+    if (taskText.length > 3 && key === 'Enter') {
+      const {addTask} = this.props
       addTask((new Date()).getTime(), taskText, false)
 
       this.setState({
@@ -38,21 +36,34 @@ class ToDo extends Component {
     }
   }
 
+  filterTasks = (tasks, activeFilter) => {
+    switch (activeFilter) {
+      case 'completed':
+        return tasks.filter(task => task.isCompleted)
+      case 'active':
+        return tasks.filter(task => !task.isCompleted)
+      default:
+        return tasks
+    }
+  }
+
   render() {
-    const {activeFilter, taskText} = this.state;
-    const { tasks, removeTask, completeTask } = this.props
+    const {taskText} = this.state;
+    const {tasks, removeTask, completeTask, filters, changeFilter} = this.props
     const isTasksExist = tasks && tasks.length > 0;
+    const filteredTask = this.filterTasks(tasks, filters)
 
     return (
         <div className="todo-wrapper">
-          <ToDoInput onKeyPress={this.addTask} onChange={this.handleInputChange} value={taskText} />
-          {isTasksExist && <ToDoList tasksList={tasks} removeTask={removeTask} completeTask={completeTask}/>}
-          {isTasksExist && <Footer amount={tasks.length} activeFilter={activeFilter}/>}
+          <ToDoInput onKeyPress={this.addTask} onChange={this.handleInputChange} value={taskText}/>
+          {isTasksExist && <ToDoList tasksList={filteredTask} removeTask={removeTask} completeTask={completeTask}/>}
+          {isTasksExist && <Footer changeFilter={changeFilter} amount={filteredTask.length} activeFilter={filters}/>}
         </div>
     );
   }
 }
 
-export default connect(state => ({
-  tasks: state.tasks,
-}), {addTask, removeTask, completeTask})(ToDo);
+export default connect(({tasks, filters}) => ({
+  tasks,
+  filters,
+}), {addTask, removeTask, completeTask, changeFilter})(ToDo);
